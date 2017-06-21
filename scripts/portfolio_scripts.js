818bd5af5614488a9ef6f62fd9f9aac9c40e6fac
Ick Hyun Kwon <kwonih1020@gmail.com>
2017-06-20 22:31:08 -0400

    moved html in main folder

--- scripts/portfolio_scripts.js
+++ scripts/portfolio_scripts.js
@@ -15,34 +15,34 @@ $(document).ready(function(){
 	});
 	$("#project_pic1").on({
         mouseenter: function() {
-            $("#project_pic1").attr("src", "./images/gif/simon_game.gif"); //on hover replace pic.png to pic.gif(animation
+            $("#project_pic1").attr("src", "../images/gif/simon_game.gif"); //on hover replace pic.png to pic.gif(animation
           },
           mouseleave: function() {
-            $("#project_pic1").attr("src", "./images/screenshots/simon_game.png"); //change pic.gif to pic.png
+            $("#project_pic1").attr("src", "../images/screenshots/simon_game.png"); //change pic.gif to pic.png
           }
         });
     $("#project_pic2").on({
         mouseenter: function() {
-            $("#project_pic2").attr("src", "./images/gif/travel_dairy.gif"); //on hover replace pic.png to pic.gif(animation
+            $("#project_pic2").attr("src", "../images/gif/travel_dairy.gif"); //on hover replace pic.png to pic.gif(animation
           },
           mouseleave: function() {
-            $("#project_pic2").attr("src", "./images/screenshots/travel_dairy.png"); //change pic.gif to pic.png
+            $("#project_pic2").attr("src", "../images/screenshots/travel_dairy.png"); //change pic.gif to pic.png
           }
         });
     $("#project_pic3").on({
         mouseenter: function() {
-            $("#project_pic3").attr("src", "./images/gif/weatro.gif"); //on hover replace pic.png to pic.gif(animation
+            $("#project_pic3").attr("src", "../images/gif/weatro.gif"); //on hover replace pic.png to pic.gif(animation
           },
           mouseleave: function() {
-            $("#project_pic3").attr("src", "./images/screenshots/weatro.png"); //change pic.gif to pic.png
+            $("#project_pic3").attr("src", "../images/screenshots/weatro.png"); //change pic.gif to pic.png
           }
         });
     $("#project_pic4").on({
         mouseenter: function() {
-            $("#project_pic4").attr("src", "./images/gif/tick_tac_toe.gif"); //on hover replace pic.png to pic.gif(animation
+            $("#project_pic4").attr("src", "../images/gif/tick_tac_toe.gif"); //on hover replace pic.png to pic.gif(animation
           },
           mouseleave: function() {
-            $("#project_pic4").attr("src", "./images/screenshots/tick_tac_toe.png"); //change pic.gif to pic.png
+            $("#project_pic4").attr("src", "../images/screenshots/tick_tac_toe.png"); //change pic.gif to pic.png
           }
         });
 });
